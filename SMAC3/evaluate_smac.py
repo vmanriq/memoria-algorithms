1a07b184be281de9268fa3825352cfb70f2c727c
Vicente Manriquez <vmanriquez@buk.cl>
2021-11-30 15:03:02 -0300

    feat: update evaluate smac

--- SMAC3/evaluate_smac.py
+++ SMAC3/evaluate_smac.py
@@ -5,9 +5,9 @@ import os
 
 
 SEED = 3
-NUMBER_OF_SEEDS = 5
+NUMBER_OF_SEEDS = 10
 ALGORITHM = '/home/manriq/Documents/universidad/TT/tunners/SMAC3/algorithms/ak/target_algorithm/AK'
-INSTANCE_FOLDER = '/home/manriq/Documents/universidad/TT/tunners/SMAC3/algorithms/instances/testing/testingEasy'
+INSTANCE_FOLDER = '/home/manriq/Documents/universidad/TT/tunners/SMAC3/algorithms/instances/testing/testingHard'
 
 random.seed(SEED)
 
@@ -16,7 +16,7 @@ random.seed(SEED)
 
 
 def exec_algorithm(algorithm, instance, params, seed):
-    TOTAL_EVAL = '3000'
+    TOTAL_EVAL = '1000'
     cmd = [algorithm, instance, str(seed), params['ants'], TOTAL_EVAL, params['alpha'], params['beta'], params['ph-max'], params['ph-min'], params['rho']]
     cmd += ['0','0','0','0','0','0','0','0']
     p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
@@ -27,6 +27,7 @@ def exec_algorithm(algorithm, instance, params, seed):
 
 
 def evaluate_params(algorithm, instance_folder, seeds_number, params):
+    resumen = []
     for filename in os.listdir(instance_folder):
         instance = os.path.join(instance_folder, filename)
         if os.path.isfile(instance):
@@ -39,8 +40,10 @@ def evaluate_params(algorithm, instance_folder, seeds_number, params):
                 print(f"VALUE: {output} ; SEED: {instance_seed}")
             values = list(map(float, values))
             promedio = sum(values)/len(values)
+            resumen.append((filename, promedio))
             print(f"El promedio es {promedio}")
-    return
+    print(resumen)
+    return resumen
 
 
 
@@ -48,9 +51,19 @@ def evaluate_params(algorithm, instance_folder, seeds_number, params):
 
 #dic_params = params_to_dict(output_smac)
 
-param_values = [{'alpha':'3.5', 'ants': '8', 'beta': '6.1', 'ph-max': '6.8', 'ph-min': '0.01', 'rho': '0.6'}]
+param_values = [
+                {'alpha': '1.6','ants': '15', 'beta': '7.6', 'ph-max': '6.7', 'ph-min': '0.01', 'rho': '0.1'},
+                {'alpha': '1.8','ants': '18', 'beta': '7.7', 'ph-max': '7.9', 'ph-min': '0.01', 'rho': '0.1'},
+                #{'alpha':'1.3', 'ants': '20', 'beta': '5.9', 'ph-max': '2.0', 'ph-min': '0.01', 'rho': '0.4'},
+                #{'alpha': '3.7','ants': '8', 'beta': '6.1', 'ph-max': '7.7', 'ph-min': '0.01', 'rho': '0.8'},
+                #{'alpha': '5.0','ants': '15', 'beta': '3.9', 'ph-max': '8.9', 'ph-min': '0.01', 'rho': '0.1'},
+               ]
 
+resumen_general = []
 for params in param_values:
     print(f"Parámetros obtenidos por SMAC {params}")
-    evaluate_params(ALGORITHM, INSTANCE_FOLDER, NUMBER_OF_SEEDS, params)
+    resumen = evaluate_params(ALGORITHM, INSTANCE_FOLDER, NUMBER_OF_SEEDS, params)
+    resumen_general.append((params, resumen))
     print("\n\n")
+
+print(resumen_general)
